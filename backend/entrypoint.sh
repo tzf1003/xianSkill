142918c85ffa738553ca146309a4db2c2bc42dee
落花人 <1244201693@qq.com>
2026-03-07 14:27:13 +0800

    修复部署

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,8 @@
-﻿#!/bin/sh
+#!/bin/sh
 set -e
 
-# 绛夊緟 Postgres 灏辩华鍚庢墽琛屾暟鎹簱杩佺Щ
+export PYTHONPATH=/app
+
 echo "[entrypoint] Running database migrations..."
 alembic upgrade head
 echo "[entrypoint] Migrations done."
